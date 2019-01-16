ed4f97a606526a5ec76515d04b58034272c55306
doug2691 <dougreuter@gmail.com>
2019-01-16 10:12:33 -0600

    hello

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = 80
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/hello/:name', function (req, res) {
+    res.send('hello ' + req.params.name + '!');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
